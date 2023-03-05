22b404b39f5300dedd62b9ac5afa64ba099fe192
Chase Montgomery <blufenix@blufenix.net>
2023-03-05 14:44:30 -0500

    cleaned up source

--- langpoc/app/src/main/java/com/samsung/SMT/lang/poc/MainActivity.java
+++ langpoc/app/src/main/java/com/samsung/SMT/lang/poc/MainActivity.java
@@ -12,13 +12,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ///data/app/com.samsung.SMT.lang.poc-ILVI78QqQZJs35VkUAQajA==/lib/arm
-       // System.loadLibrary("mstring");
-
         Intent intent = new Intent();
         intent.setComponent(new ComponentName("com.samsung.SMT", "com.samsung.SMT.SamsungTTSService"));
         startService(intent);
-
-
     }
 }
